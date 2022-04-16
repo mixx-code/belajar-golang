dbc4c4866ab0c3af2aa765199d36d1d53578716b
Rizki febriansyah <rfebriansyah385@gmail.com>
2022-04-16 13:39:09 +0700

    belajar anonymous function

---
+++ function/anonymous-function.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Blacklist func(string) bool
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("you are blocked", name)
+	} else {
+		fmt.Println("welcome", name)
+	}
+}
+
+func main() {
+	blacklist := func(name string) bool {
+		return name == "admin"
+	}
+
+	registerUser("admin", blacklist)
+	registerUser("rizki", blacklist)
+}
